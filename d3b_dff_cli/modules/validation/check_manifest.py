8c9b87f8c628406b8d3736d4a5d52448b7200e5e
HuangXiaoyan0106 <xiaoyanhuang0106@gmail.com>
2024-08-23 22:13:48 +0800

    :pencil2: fix typo

--- d3b_dff_cli/modules/validation/check_manifest.py
+++ d3b_dff_cli/modules/validation/check_manifest.py
@@ -54,13 +54,13 @@ def validate_row(row, rules):
                             minum_value = float(greater_than_value.rstrip("M"))*1000_000 # Other experimental strategy should be greater than the specified value.
                         
                         if pd.notna(cell_value):
-                            size_byte = float(cell_value)
                             try:
+                                size_byte = float(cell_value)
                                 if size_byte < minum_value:
                                     error_messages.append(f"Warning: *{col}* less than {greater_than_value}")
 
                             except ValueError:
-                                error_messages.append(f"*{col}*: {size_byte} is not a valid value")
+                                error_messages.append(f"*{col}*: {cell_value} is not a valid value")
 
     if error_messages:
         return False, error_messages  # Return all error messages for this row
