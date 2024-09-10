444cbda288878ea20444ac4b658b3f5a92a1b547
HuangXiaoyan0106 <xiaoyanhuang0106@gmail.com>
2024-09-10 18:05:42 +0800

    :technologist: update rules

--- d3b_dff_cli/modules/validation/cerberus_custom_checks.py
+++ d3b_dff_cli/modules/validation/cerberus_custom_checks.py
@@ -1,69 +1,40 @@
 from cerberus import Validator
-import pandas as pd
 import warnings
 
 # Suppress specific UserWarnings from Cerberus
 warnings.filterwarnings("ignore", category=UserWarning, module="cerberus.validator")
+
 class CustomValidator(Validator):
     def __init__(self, schema, rules=None, *args, **kwargs):
+        """
+        Initialize the CustomValidator with schema and optional custom rules.
+        """
         super().__init__(schema, *args, **kwargs)
-        self.rules = rules if rules else {}
+        self.custom_rules = rules or {}
 
-    def _validate_dependencies(self, dependencies, field, value):
+    def _check_dependencies(self, field, document):
         """
-        Check if the field's dependencies are met. If not, skip validation for this field.
+        Check if the field's dependencies are met.
         """
+        dependencies = self.schema.get(field, {}).get('dependencies', {})
         for dependency_field, allowed_values in dependencies.items():
-            dependency_value = self.document.get(dependency_field)
-
-            # Check if the allowed_values is a list or a single value
+            dependency_value = document.get(dependency_field)
             if isinstance(allowed_values, list):
                 if dependency_value not in allowed_values:
-                    return False  # Dependencies not met
+                    return False
             else:
                 if dependency_value != allowed_values:
-                    return False  # Dependencies not met
-        
-        # Dependencies are met, perform validation
+                    return False
         return True
 
-    def _validate_field(self, field, value, field_schema):
+    def _validate_custom_rules(self, field, value):
         """
-        Validate the field value based on its schema.
+        Apply custom validation rules that are beyond the default Cerberus validation.
         """
-        field_type = field_schema.get('type')
-        allowed_values = field_schema.get('allowed')
-
-        # Validate required fields
-        if field_schema.get('required') and (value is None or pd.isna(value) or value == ''):
-            self._error(field, f"{field} is required.")
-            return False
-
-        # Handle validation for allowed values
-        if allowed_values and value not in allowed_values:
-            self._error(field, f"{field} must be one of {allowed_values}.")
-            return False
-        
-        # Validate type
-        if field_type:
-            if field_type == 'boolean':
-                if value not in ['true', 'false']:
-                    self._error(field, f"{field} must be a boolean value.")
-                    return False
-            elif field_type == 'integer':
-                if not (isinstance(value, int) or (isinstance(value, float) and value.is_integer())):
-                    self._error(field, f"{field} must be of integer type.")
-                    return False
-            elif field_type == 'string':
-                if not isinstance(value, str):
-                    self._error(field, f"{field} must be of string type.")
-                    return False
-                
-        # Apply additional validation based on custom rules
         if field == 'file_name':
             file_format = self.document.get('file_format')
             if file_format:
-                extensions = self.rules.get('file_name_extensions')
+                extensions = self.custom_rules.get('file_name_extensions', {})
                 expected_extension = extensions.get(file_format)
                 if expected_extension and not value.lower().endswith(expected_extension):
                     self._error(field, f"{field} must end with {expected_extension} for file_format '{file_format}'.")
@@ -72,39 +43,42 @@ class CustomValidator(Validator):
         if field == 'file_size':
             file_format = self.document.get('file_format')
             experiment = self.document.get("experiment_strategy")
-            
-            byte_cutoff_general = self.rules.get('file_size_byte_cutoff').get('general_cutoff')
-            byte_cutoff_wgs_wxs = self.rules.get('file_size_byte_cutoff').get('wgs_wxs_cutoff')
-            dependencies_format = self.rules.get('file_size_byte_cutoff').get('dependencies').get('file_format')
+            byte_cutoff_general = self.custom_rules.get('file_size_byte_cutoff', {}).get('general_cutoff', 0)
+            byte_cutoff_wgs_wxs = self.custom_rules.get('file_size_byte_cutoff', {}).get('wgs_wxs_cutoff', 0)
+            dependencies_format = self.custom_rules.get('file_size_byte_cutoff', {}).get('dependencies', {}).get('file_format', [])
 
-            if experiment in ["wgs", "wxs", "wes"]:
-                minum_value = byte_cutoff_wgs_wxs
-            else:
-                minum_value = byte_cutoff_general
-            
+            minum_value = byte_cutoff_wgs_wxs if experiment in ["wgs", "wxs", "wes"] else byte_cutoff_general
+
             if file_format in dependencies_format:
                 if value < minum_value:
-                    self._error(field, f"Warning: *{field}* must be at least {minum_value} for file_format '{file_format}'.")
+                    self._error(field, f"[Warning] must be at least {minum_value} for file_format '{file_format}'.")
                     return False
+
         return True
 
+    
     def validate(self, document, *args, **kwargs):
         """
-        Override validate method to ensure dependencies are respected.
+        Override validate method to first check dependencies, then apply default and custom validation.
         """
         self.document = document
-        is_valid = True
-        
-        for field, field_schema in self.schema.items():
-            value = document.get(field)
-            dependencies = field_schema.get('dependencies')
-            
-            if dependencies:
-                if not self._validate_dependencies(dependencies, field, value):
-                    continue  # Skip validation if dependencies are not met
 
-            # Perform validation if dependencies are met
-            if not self._validate_field(field, value, field_schema):
-                is_valid = False
+        # Prepare filtered document with fields that meet dependencies
+        filtered_document = {}
+        for field in self.schema:
+            if self._check_dependencies(field, document):
+                filtered_document[field] = document.get(field)
+
+        # Perform default validation
+        super().validate(filtered_document, *args, **kwargs)
+
+        for field, value in filtered_document.items():
+            self._validate_custom_rules(field, value)
         
-        return is_valid
+        # Filter and return errors
+        print_errors = {field: errors for field, errors in self.errors.items() if field in filtered_document}
+
+        # Determine overall validity based on errors
+        is_valid = not bool(print_errors)
+
+        return is_valid, print_errors
--- d3b_dff_cli/modules/validation/check_manifest.py
+++ d3b_dff_cli/modules/validation/check_manifest.py
@@ -8,7 +8,9 @@ wk_dir = os.path.dirname(os.path.abspath(__file__))
 validation_schema = os.path.join(wk_dir, "validation_rules_schema.json")
 
 def load_data(manifest_file):
-    # Load data based on file extension
+    """
+    Load data from a manifest file and convert strings to lowercase.
+    """
     file_extension = manifest_file.split('.')[-1].lower()
     if file_extension == 'csv':
         manifest_data = pd.read_csv(manifest_file)
@@ -25,12 +27,13 @@ def load_data(manifest_file):
     else:
         raise ValueError("Unsupported file format. Please provide a CSV, TSV, or Excel file.")
     
-    # Convert manifest to lowercase
-    manifest_data = manifest_data.apply(lambda col: col.astype(str).str.lower() if col.dtype.name in ['object', 'bool'] else col)
-
+    manifest_data = manifest_data.apply(lambda col: col.astype(str).str.lower() if col.dtype.name in ['object'] else col)
     return manifest_data
 
 def convert_schema_to_lowercase(schema):
+    """
+    Convert all string values in the schema to lowercase.
+    """
     for k, v in schema.items():
         if isinstance(v, dict):
             convert_schema_to_lowercase(v)
@@ -41,48 +44,56 @@ def convert_schema_to_lowercase(schema):
     return schema
 
 def validate_data(df, schema_json):
+    """
+    Validate the DataFrame against the schema.
+    """
     valid = True
     errors = []
-    custom_rules = schema_json['custom_rules']
+    custom_rules = schema_json.get('custom_rules', {})
     
     for index, row in df.iterrows():
-
-        # Check the manifest type and select the appropriate validation rules
-        experiment_strategy = row["experiment_strategy"]
-        platform = row["platform"]
+        experiment_strategy = row.get("experiment_strategy", "").lower()
+        platform = row.get("platform", "").lower()
+        
         if platform == "pacbio":
             rule_type = "pacbio_longread_rules"
         else:
-            if experiment_strategy in ["wgs","wxs","wes","target sequencing","panel","target"]:
+            if experiment_strategy in ["wgs", "wxs", "wes", "target sequencing", "panel", "target"]:
                 rule_type = "DNAseq_rules"
-            elif experiment_strategy in ["rna-seq","rnaseq","mirna-seq","mirnaseq"]:
+            elif experiment_strategy in ["rna-seq", "rnaseq", "mirna-seq", "mirnaseq"]:
                 rule_type = "RNAseq_rules"
-            elif experiment_strategy in ["scrna-seq","snran-seq","scrnaseq","snranseq"]:
+            elif experiment_strategy in ["scrna-seq", "snran-seq", "scrnaseq", "snranseq"]:
                 rule_type = "single_cell_rules"
-            elif experiment_strategy in ["methtlation","methylation microarray"]:
+            elif experiment_strategy in ["methtlation", "methylation microarray"]:
                 rule_type = "methylation_rules"
             else:
-                raise ValueError("Unsupported experiment_strategy for Row {0}".format(index + 1))
-        schema = schema_json[rule_type]
+                raise ValueError(f"Unsupported experiment_strategy for Row {index + 1}")
         
-        # Filter out fields not in the schema fields
+        schema = schema_json.get(rule_type, {})
+        
+        # Filter out fields not in the schema fields for combined manifest
         row_dict = row.to_dict()
         filtered_row_dict = {k: v for k, v in row_dict.items() if k in schema}
         
         v = CustomValidator(schema, custom_rules)
-        is_valid = v.validate(filtered_row_dict)
+        (is_valid, out_error) = v.validate(filtered_row_dict)
+        
         if not is_valid:
             valid = False
             errors.append({
                 'row': index + 1,
-                'errors': v.errors
+                'errors': out_error
             })
     
     return valid, errors
 
 def main(args):
+    """
+    Main function to load schema, validate data, and print the validation report.
+    """
     with open(validation_schema, 'r') as f:
         schema = json.load(f)
+    
     schema_json = convert_schema_to_lowercase(schema)
 
     # Load and preprocess the data
@@ -90,7 +101,7 @@ def main(args):
 
     # Validate the data
     valid, errors = validate_data(df, schema_json)
-    
+
     # Print validation report
     if valid:
         print("====Validation Passed====\n  All rows are valid.")
@@ -98,7 +109,6 @@ def main(args):
         # Check if all warning messages
         only_warnings = all("Warning" in field_error for error in errors for field_errors in error['errors'].values() for field_error in field_errors)
 
-        # Print appropriate message
         if only_warnings:
             print("====Validation Warnings====")
         else:
@@ -107,7 +117,8 @@ def main(args):
         for error in errors:
             print(f"Row {error['row']}:")
             for field, field_errors in error['errors'].items():
-                print(f"  {field_errors[0]}")
+                for field_error in field_errors:
+                    print(f"  {field}: {field_error}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Validate a manifest based on defined rules.")
